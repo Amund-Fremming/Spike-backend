ed47ab2dad26c5b06e8a911126221bab323d1ee2
amund-fremming <amund.fremming@gmail.com>
2024-01-15 18:24:19 +0100

    New environment variables

--- Program.cs
+++ Program.cs
@@ -14,11 +14,13 @@ builder.Services.AddScoped<QuestionRepo>();
 // Connects to database
 builder.Services.AddDbContext<Data.AppDbContext>(options =>
 {
+    var connectionString = Environment.GetEnvironmentVariable("DATABASE_CONNECTION_STRING");
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
 var app = builder.Build();
 
+// Does not enable Swagger for production
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
