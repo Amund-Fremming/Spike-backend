21703538a84f25568e0356b0ece3528449c263c6
amund-fremming <amund.fremming@gmail.com>
2024-02-05 11:18:20 +0100

    Fixed bugs with the methods

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f2129fd8c7c0e3c3ad2a27a1e0873b5095693aac")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+271a8463ab4fdcf202352920d9020c3bee639bf0")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
