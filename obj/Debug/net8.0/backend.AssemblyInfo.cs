8eca99ea94adbeb670ba7bc20ed36cb4f3cc2c11
amund.fremming <amund.fremming@gmail.com>
2024-06-01 14:14:53 +0200

    New hosting server

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6cc9ff06441f422273704118a576a746e8a139a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a46a17d9c257e38c005c064cbc0a34965574754c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
